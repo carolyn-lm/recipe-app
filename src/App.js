c6b6e2d9efba27347714c7af11805a8d8c02cb26
carolyn_m <carolynlmadden@gmail.com>
2025-05-08 09:13:29 -0400

    Added New recipe form

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import Header from "./components/Header";
 import "./App.css";
 import RecipeExcerpt from "./components/RecipeExcerpt";
 import RecipeFull from "./components/RecipeFull";
+import NewRecipeForm from "./components/NewRecipeForm";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
   const [selectedRecipe, setSelectedRecipe] = useState(null);
+  const [newRecipe, setNewRecipe] = useState({
+    title: "",
+    ingredients: "",
+    instructions: "",
+    servings: 1, // conservative default
+    description: "",
+    image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" //default
+  });
+  const [showNewRecipeForm, setShowNewRecipeForm] = useState(false);
 
   useEffect(() => {
     fetchAllRecipes();
@@ -35,17 +45,30 @@ function App() {
     setSelectedRecipe(null);
   }
 
+  const showRecipeForm = () => {
+    setShowNewRecipeForm(true);
+    setSelectedRecipe(null);
+  }
+
+  const hideRecipeForm = () => {
+    setShowNewRecipeForm(false);
+  }
+
+  const onUpdateForm = (e) => {
+    const { name, value } = e.target;
+    setNewRecipe({ ...newRecipe, [name]: value });
+  }
+
   return (
     <div className='recipe-app'>
-      <Header />
-      {selectedRecipe ?
-        (<RecipeFull selectedRecipe={selectedRecipe} handleUnselectRecipe={handleUnselectRecipe} />)
-        :
-        (
-          <div className="recipe-list">
-            {recipes.map(recipe => <RecipeExcerpt key={recipe.id} recipe={recipe} handleSelectRecipe={handleSelectRecipe} />)}
-          </div>
-        )}
+      <Header showRecipeForm={showRecipeForm} />
+      {showNewRecipeForm && <NewRecipeForm newRecipe={newRecipe} hideRecipeForm={hideRecipeForm} onUpdateForm={onUpdateForm} />}
+      {selectedRecipe && <RecipeFull selectedRecipe={selectedRecipe} handleUnselectRecipe={handleUnselectRecipe} />}
+      {!selectedRecipe &&
+        <div className="recipe-list">
+          {recipes.map(recipe => <RecipeExcerpt key={recipe.id} recipe={recipe} handleSelectRecipe={handleSelectRecipe} />)}
+        </div>
+      }
     </div>
   );
 }
